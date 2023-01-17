45f476b65e3dcc415744969f0a1c68aacd5a6d49
mubasshir29 <mubasshir.f29>
2023-01-17 15:44:45 +0400

    17/01/2023

--- client/src/NewsCard/NewsCard.jsx
+++ client/src/NewsCard/NewsCard.jsx
@@ -8,11 +8,11 @@ function NewsCard({data}) {
         <img src={data.image} alt='sachin_sehwag_picture'></img>
         <div className='card_right'>
             <div className='card_right_content'>
-                <h2 className='card_title'>{data.title}</h2>
+                <h3 className='card_title'>{data.title}</h3>
                 <p className='card_author_content' >{data.created_at} <span className='card_author bold'>{data.author}</span> بہ تحریر</p>
                 <span className='card_content'>{data.content}</span>
             </div>
-            <a className='card_right_ref_link' href=''> <span  className='bold'>{data.reference_name}</span> مزید تفصیل سے پڑھیں </a>
+            <a className='card_right_ref_link' href=''> <span  className='bold'> {data.reference_name}</span> پر مزید تفصیل سے پڑھیں </a>
         </div>
     </div>
   )
